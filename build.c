bf4d7f1532e06aef063d67bff598752f4c8f0028
xt9y <felix@xt9y.de>
2026-09-09 00:36:01 +0200

    test: wire dense Earth sphere contract

--- build.c
+++ build.c
@@ -58,6 +58,9 @@ void build(C_Build *b)
     C_Target *earth_texture_contract = c_test(b, "earth-texture-contract");
     configureViewer(earth_texture_contract, horse, "tests/earth_texture_contract.cpp");
 
+    C_Target *earth_sphere_contract = c_test(b, "earth-sphere-contract");
+    configureViewer(earth_sphere_contract, horse, "tests/earth_sphere_contract.cpp");
+
     C_Target *earth_sun_orbit_contract = c_test(b, "earth-sun-orbit-contract");
     c_sources(earth_sun_orbit_contract, "tests/earth_sun_orbit_contract.cpp");
     c_flag(earth_sun_orbit_contract, "-std=c++20");
