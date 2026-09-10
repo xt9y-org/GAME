d09d86c23ef076161828626a8284053ed92c88b8
xt9y <felix@xt9y.de>
2026-09-11 00:10:38 +0200

    expose scene debug and metrics hooks

--- Scenes/Scene.hpp
+++ Scenes/Scene.hpp
@@ -4,7 +4,9 @@
 #include "Sources/Ecs/Ecs.hpp"
 
 #include <cstddef>
+#include <functional>
 #include <string>
+#include <string_view>
 
 namespace Game::Scenes {
 
@@ -22,6 +24,14 @@ public:
 
     virtual Ecs::Entity camera() const = 0;
     virtual std::size_t triangleCount() const = 0;
+
+    virtual bool usesFreeCamera() const { return true; }
+    virtual bool showStats() const { return true; }
+    virtual void drawDebug(Ecs::World& world) { (void)world; }
+    virtual void emitMetrics(const std::function<void(std::string_view, double)>& emit) const
+    {
+        (void)emit;
+    }
 };
 
 } // namespace Game::Scenes
