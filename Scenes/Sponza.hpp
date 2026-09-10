ce836fbd57b6b7af001416bc01e2f62d218e8bc6
xt9y <felix@xt9y.de>
2026-09-10 17:37:26 +0200

    scenes: keep Sponza renderer agnostic

--- Scenes/Sponza.hpp
+++ Scenes/Sponza.hpp
@@ -5,6 +5,7 @@
 
 #include "Sources/Camera.hpp"
 #include "Sources/Models/Models.hpp"
+#include "Sources/Renderer/Components.hpp"
 
 #include <algorithm>
 #include <cmath>
@@ -18,15 +19,6 @@ public:
     const char *name() const override
     {
         return "Sponza";
-    }
-
-    void configure(Renderers& renderers) override
-    {
-        renderers.ray_tracer.settings().resolution_divisor = 4;
-        renderers.ray_tracer.settings().exposure = 1.05f;
-        renderers.path_tracer.settings().resolution_divisor = 2;
-        renderers.path_tracer.settings().samples_per_frame = 2;
-        renderers.path_tracer.settings().exposure = 1.05f;
     }
 
     bool load(Ecs::World& world, std::string& error) override
