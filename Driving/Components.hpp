15d987feb65872d0c0657dbb91808753d2faa12f
xt9y <felix@xt9y.de>
2026-09-11 10:43:29 +0200

    driving: remove traffic component

--- Driving/Components.hpp
+++ Driving/Components.hpp
@@ -42,18 +42,6 @@ struct DrivingCamera {
     float vibration_phase = 0.0f;
 };
 
-struct Traffic {
-    int lane = 0;
-    int target_lane = 0;
-    float speed = 30.0f;
-    float desired_speed = 30.0f;
-    float length = 4.4f;
-    float lane_change_speed = 1.8f;
-    float follow_distance = 34.0f;
-    float lane_change_cooldown = 0.0f;
-    bool heavy = false;
-};
-
 struct RoadPiece {};
 
 } // namespace Game::Driving
