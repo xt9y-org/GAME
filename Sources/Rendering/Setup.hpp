61e53777406d170cdbdf7351e35cf5f83d2a37f3
xt9y <felix@xt9y.de>
2026-09-18 13:42:55 +0200

    Add renderer setup API

---
+++ Sources/Rendering/Setup.hpp
@@ -0,0 +1,14 @@
+#ifndef GAME_RENDERING_SETUP_HPP
+#define GAME_RENDERING_SETUP_HPP
+
+namespace Renderer {
+class Manager;
+}
+
+namespace Rendering {
+
+void configure(Renderer::Manager& renderers);
+
+} // namespace Rendering
+
+#endif
