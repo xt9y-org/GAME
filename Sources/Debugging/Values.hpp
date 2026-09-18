52ffebb8e8e74c206b74805ecf7e6e14675f6952
xt9y <felix@xt9y.de>
2026-09-19 01:13:09 +0200

    Add ambient occlusion debug ranges

--- Sources/Debugging/Values.hpp
+++ Sources/Debugging/Values.hpp
@@ -58,6 +58,9 @@ inline constexpr Range<float> ShadowBias{0.0f, 0.05f, 0.0001f};
 
 inline constexpr Range<int> RendererResolutionDivisor{1, 8, 1};
 
+inline constexpr Range<float> AmbientOcclusionStrength{0.0f, 3.0f, 0.05f};
+inline constexpr Range<float> AmbientOcclusionRadius{0.05f, 10.0f, 0.05f};
+
 inline constexpr Range<float> GaussianRadius{0.5f, 6.0f, 0.1f};
 inline constexpr Range<float> GaussianMinimumDepthEpsilon{0.0f, 0.05f, 0.0005f};
 inline constexpr Range<float> GaussianRelativeDepthEpsilon{0.0f, 0.01f, 0.0001f};
