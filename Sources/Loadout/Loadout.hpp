ed78df89485624b1b536268b28bcb1f326634522
xt9y <felix@xt9y.de>
2026-09-18 12:38:05 +0200

    Keep fire control separate from loadout state

--- Sources/Loadout/Loadout.hpp
+++ Sources/Loadout/Loadout.hpp
@@ -1,8 +1,6 @@
 #ifndef GAME_LOADOUT_LOADOUT_HPP
 #define GAME_LOADOUT_LOADOUT_HPP
 
-#include "Fire.hpp"
-
 #include <Ecs/Ecs.hpp>
 #include <Renderer/ModelScene.hpp>
 
@@ -40,7 +38,6 @@ struct WeaponItem
     std::filesystem::path shoot;
     std::filesystem::path reload;
     std::filesystem::path inspect;
-    Fire::Profile fire;
 };
 
 struct State
@@ -62,7 +59,6 @@ struct State
     Models::ModelHandle reload = Models::INVALID_MODEL;
     Models::ModelHandle inspect = Models::INVALID_MODEL;
 
-    Fire::State fire;
     bool shooting = false;
     std::string error;
 };
@@ -80,14 +76,6 @@ bool init(
 bool selectWeapon(State& state, Ecs::World& world, std::size_t index);
 bool selectArms(State& state, Ecs::World& world, std::size_t index);
 
-bool fire(
-    State& state,
-    bool pressed,
-    bool held,
-    float delta_seconds,
-    std::string *error = nullptr
-);
-bool toggleFireMode(State& state);
 bool shoot(State& state, std::string *error = nullptr);
 bool reload(State& state, std::string *error = nullptr);
 bool inspect(State& state, std::string *error = nullptr);
