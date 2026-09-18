18d3066a2188598f557d53a1c1fdb877464af28d
xt9y <felix@xt9y.de>
2026-09-18 16:41:37 +0200

    Configure renderer feature defaults

--- Sources/Rendering/Setup.cpp
+++ Sources/Rendering/Setup.cpp
@@ -2,8 +2,11 @@
 
 #include "../Debugging/Values.hpp"
 
+#include <Renderer/Features.hpp>
+#include <Renderer/GaussianSplat/GaussianSplat.hpp>
 #include <Renderer/GlobalIllumination/GlobalIllumination.hpp>
 #include <Renderer/Manager.hpp>
+#include <Renderer/Quality.hpp>
 #include <Renderer/Rasterizer/Rasterizer.hpp>
 #include <Renderer/Volumetrics/Volumetrics.hpp>
 
@@ -14,17 +17,25 @@ void configure(Renderer::Manager& renderers)
     auto& rasterizer = renderers.add<Renderer::Rasterizer>("Rasterizer");
     rasterizer.setEnabled(true);
     rasterizer.setViewportCulling(true);
-    rasterizer.setShadowResolution(Debugging::Values::ShadowResolutionDefault);
-    rasterizer.setShadowCascades(Debugging::Values::ShadowCascadesDefault);
+    rasterizer.setShadowQuality(Renderer::Quality::High);
     rasterizer.setShadowDistance(Debugging::Values::ShadowDistanceDefault);
     rasterizer.setShadowNearPlane(Debugging::Values::ShadowNearDefault);
     rasterizer.setClearColor({0.0f, 0.0f, 0.0f, 0.0f});
 
     Renderer::GlobalIllumination::settings() =
         Renderer::GlobalIllumination::Settings{};
+    Renderer::GlobalIllumination::setQuality(Renderer::Quality::High);
 
     Renderer::Volumetrics::settings() = Renderer::Volumetrics::Settings{};
-    Renderer::Volumetrics::settings().enabled = false;
+    Renderer::Volumetrics::setQuality(Renderer::Quality::High);
+
+    Renderer::GaussianSplat::settings() = Renderer::GaussianSplat::Settings{};
+
+    Renderer::Features::Settings& features = Renderer::Features::settings();
+    features = Renderer::Features::Settings{};
+    features.global_illumination = false;
+    features.volumetrics = false;
+    features.gaussian_splat = false;
 }
 
 } // namespace Rendering
